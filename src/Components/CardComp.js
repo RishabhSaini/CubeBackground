7d5e9285853f33cd7a8c03236f2c289f00c6c2f9
RishabhSaini <rishabhsaini01@gmail.com>
2021-01-09 14:31:03 -0500

    COmpleting the modalboxes

--- src/Components/CardComp.js
+++ src/Components/CardComp.js
@@ -1,36 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CardComp.css";
 import "w3-css/w3.css";
-import Typical from "react-typical";
+import ModalPopup from "./ModalPopup";
+import styled from "styled-components";
 
 const CardComp = () => {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal((prev) => !prev);
+  };
   return (
     <React.Fragment>
       <div className="header">
-        <Typical
-          steps={[
-            "Welcome",
-            1000,
-            "I am Rishabh",
-            2000,
-            "I am a Designer",
-            1000,
-            "I am a Creator",
-            1000,
-            "I am a Thinker",
-            1000,
-            "I am a Developer",
-            1000,
-            "I am a Innovator",
-            1000,
-            "I am a Inventor",
-            1000,
-          ]}
-          loop={Infinity}
-          wrapper="h1"
-          className=""
-        />
+        <div className="w3-container">
+          <StyledButton onClick={openModal}>About me</StyledButton>
+          <StyledButton>Work Experience</StyledButton>
+        </div>
       </div>
+      <ModalPopup
+        showModal={showModal}
+        setShowModal={setShowModal}
+      ></ModalPopup>
       <div className="cardComp">
         <div className="w3-container">
           <div className="box w3-panel w3-card">
@@ -39,14 +29,22 @@ const CardComp = () => {
             Albert Einstein
             <h3>What is my aim?</h3>
             <ul className="cust-ul">
-                <li>Leverage the power of technology to create paradigm shifts in the way the society thinks</li>
-                <li>Creating solutions that make a meaningful impact  on society</li>
+              <li>
+                Leverage the power of technology to create paradigm shifts in
+                the way the society thinks
+              </li>
+              <li>
+                Creating solutions that make a meaningful impact on society
+              </li>
             </ul>
             <h3>Who am I?</h3>
             <ul className="cust-ul">
-                <li>"I think, therefore I am" René Descartes</li>
-                <li>Education: Youtube, Google, Coursera</li>
-                <li>2nd Year Computer Engineering Undergraduate at University of Toronto</li>
+              <li>"I think, therefore I am" René Descartes</li>
+              <li>Education: Youtube, Google, Coursera</li>
+              <li>
+                2nd Year Computer Engineering Undergraduate at University of
+                Toronto
+              </li>
             </ul>
             <h3>Why a website?</h3>
             To showcase my portfolio and ongoing projects
@@ -58,3 +56,15 @@ const CardComp = () => {
 };
 
 export default CardComp;
+
+const StyledButton = styled.button`
+  min-width: 100px;
+  padding: 5px 15px;
+  margin: 10px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`;
